87039ab9076aa2660f8bd6cc8b5445764802c664
ПК <>
2017-03-10 22:23:12 +0500

    ValidationForm

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(function(){
-	
+	$('.collapse').on('show.bs.collapse', function(){
+    $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+  });
+
+  $('.collapse').on('hide.bs.collapse', function(){
+    $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+  });
 });
 
 
