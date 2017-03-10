dce15ad6150d6945581735cb1efccaf2c8b58367
ПК <>
2017-03-10 13:39:15 +0500

    Initial

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(function(){
+	
+});
+
+
+$(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+    
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+    /*
+    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+    * dn - определяем положение блока на странице
+    */
+    
+    $('html, body').animate({scrollTop: dn}, 1000);
+    
+    /*
+    * 1000 скорость перехода в миллисекундах
+    */
+  });
+});
+ 
+ $('.animated-appearance').boxLoader({
+    direction:"y",
+    position: "100%",
+    effect: "fadeIn",
+    duration: "0.5s",
+    windowarea: "50%"
+});
+
