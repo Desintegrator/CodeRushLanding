02673100a02da29cda02be3484fdf07b0b8989bf
ПК <>
2017-03-12 00:47:20 +0500

    validation2

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,49 @@
-$(function(){
-	$('.collapse').on('show.bs.collapse', function(){
+$(function () {
+  $('.collapse').on('show.bs.collapse', function () {
     $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
   });
-
-  $('.collapse').on('hide.bs.collapse', function(){
+  $('.collapse').on('hide.bs.collapse', function () {
     $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
   });
+  $('#myForm').validator({
+    feedback: {
+      success: 'fa fa-check-circle',
+      error: 'fa fa-times-circle'
+    }
+  });
+  $('.form-sel').select2({
+    minimumResultsForSearch: Infinity
+  });
+
+
 });
 
 
-$(function(){
-  $('a[href^="#"]').on('click', function(event) {
+$(function () {
+  $('a[href^="#"]').on('click', function (event) {
     // отменяем стандартное действие
     event.preventDefault();
-    
+
     var sc = $(this).attr("href"),
-        dn = $(sc).offset().top;
+      dn = $(sc).offset().top;
     /*
     * sc - в переменную заносим информацию о том, к какому блоку надо перейти
     * dn - определяем положение блока на странице
     */
-    
-    $('html, body').animate({scrollTop: dn}, 1000);
-    
+
+    $('html, body').animate({ scrollTop: dn }, 1000);
+
     /*
     * 1000 скорость перехода в миллисекундах
     */
   });
 });
- 
- $('.animated-appearance').boxLoader({
-    direction:"y",
-    position: "100%",
-    effect: "fadeIn",
-    duration: "0.5s",
-    windowarea: "50%"
+
+$('.animated-appearance').boxLoader({
+  direction: "y",
+  position: "100%",
+  effect: "fadeIn",
+  duration: ".7s",
+  windowarea: "50%"
 });
 
